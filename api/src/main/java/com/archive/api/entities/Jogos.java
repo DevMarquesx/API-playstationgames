3bad68b9584d75899aea4e63afbec1adf3aa901b
Daniel <dogualtieroni@gmail.com>
2025-05-20 15:41:04 -0300

    Retirando Lombok e adicionando CONSTRUTORES E GETTERS/SETTERS

--- api/src/main/java/com/archive/api/entities/Jogos.java
+++ api/src/main/java/com/archive/api/entities/Jogos.java
@@ -10,8 +10,7 @@ import java.util.Date;
 
 @Document(collection = "jogos")
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Jogos {
     @Id
     private String id;
@@ -21,4 +20,73 @@ public class Jogos {
     private String genero;
     private Date lancamento;
     private String img;
+
+    public Jogos(String id, String nome, String descricao, String desenvolvedora, String genero, Date lancamento, String img) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.desenvolvedora = desenvolvedora;
+        this.genero = genero;
+        this.lancamento = lancamento;
+        this.img = img;
+    }
+
+    public Jogos() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDesenvolvedora() {
+        return desenvolvedora;
+    }
+
+    public void setDesenvolvedora(String desenvolvedora) {
+        this.desenvolvedora = desenvolvedora;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public Date getLancamento() {
+        return lancamento;
+    }
+
+    public void setLancamento(Date lancamento) {
+        this.lancamento = lancamento;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
 }
